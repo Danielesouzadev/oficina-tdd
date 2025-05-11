732c1a6f5856332a756ee8c5fb2e0e333188d513
Daniele Souza <contatos.danielesouza@gmail.com>
2025-05-11 20:13:35 -0300

    test: implementacao tdd

--- queue.js
+++ queue.js
@@ -3,8 +3,34 @@ class Queue {
   constructor(){
     this.elements = []
   }
+
+  size(){
+    return this.elements.length;
+  }
+
+  add(item){
+    this.elements.push(item);
+  }
+
+  peek(){
+    return this.elements[0];
+  }
+
+
+ /* dequeue(){
+    this.elements.splice(1, 0);
+  }
+  Primeiro eu utilizei o splice, que é o conhecimento que possuo
+  referente a array para remover itens. Mas ao pesquisar sobre dequeue 
+  entendi que a melhor alternativa para remover o primeiro elemento é
+  o método shift. As duas formas retornou PASS, mas decidir subir o código 
+  com o método shift.
+  */
+
+  dequeue(){
+    return this.elements.shift();
+  }
   
-  // Implementar aqui
 }
 
 module.exports = Queue
--- queue.test.js
+++ queue.test.js
@@ -3,23 +3,26 @@ const Queue = require('./queue')
 describe('Queue', () => {
   it('Deve criar uma fila', () => {
     const queue = new Queue()
-
     expect(queue).toEqual({ elements: []})
   })
 
   it('Deve ser capaz de calcular o tamanho da fila', () => {
-    // Seu código aqui
+    const queue = new Queue()
+    expect(queue.size()).toEqual(0);
   })
 
   it('Deve ser capaz de adicionar um item na fila', () => {
-   // Seu código aqui
+    const queue = new Queue()
+    expect(queue.push).toEqual();
   })
 
   it('Deve escolher o primeiro item da fila', () => {
-    // Seu código aqui
+    const queue = new Queue()
+    expect(queue.peek()).toEqual();
   })
 
   it('Deve remover o primeiro item da fila', () => {
-    // Seu código aqui
+    const queue = new Queue()
+    expect(queue.dequeue()).toEqual();
   })
 })
